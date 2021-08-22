231006d474d19cc410ba07a1981362a820a2b690
Peter Bacenet <peterbacenet@icloud.com>
2021-08-22 03:40:35 -0400

    commit tree 84ac7b6068bfb8104aaa17b4b2871ab9c31e6048

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(names, string) {
+    let correctName = names.filter(anything => anything.toUpperCase() == string.toUpperCase())
+    return correctName;
+}
+function fuzzyMatch(drivers, string) {
+    let correctLetters = drivers.filter(driver => driver.slice(0,string.length) == string)
+    return correctLetters
+}
+
+function matchName(array,string) {
+    let property = array.filter(driver => driver.name === string)
+    return property
+}
+
+
+// findMatching()
+//       1) returns all drivers that match the passed in name
+//       2) returns matching drivers if case does not match but letters do
+//       3) returns an empty array if there is no match
